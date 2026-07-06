48898cc4b49d61e818d3af04abb766b06f977e0c
siddharthtyagiii0 <your-email@example.com>
2026-07-06 14:30:00 +0530

    Added solutions for question 5 and question 17

--- Test2/quest05.py
+++ Test2/quest05.py
@@ -3,9 +3,15 @@ Create an empty stack and push the values 5, 10, 15, 20 into it. Display
 the stack.
 '''
 
-stack = []
-stack.append(5)
-stack.append(10)
-stack.append(15)
-stack.append(20)
-print("Stack:", stack)
+class Stack:
+    def __init__(self):
+        self.stack = []
+
+s = Stack()
+
+s.stack.append(5)
+s.stack.append(10)
+s.stack.append(15)
+s.stack.append(20)
+
+print("Stack:", s.stack)
--- Test2/quest17.py
+++ Test2/quest17.py
@@ -6,3 +6,13 @@ Create an undirected graph with the following edges:
 * C – D
 Display the adjacency list of the graph.
 '''
+graph = {}
+
+graph["A"] = ["B", "C"]
+graph["B"] = ["A", "D"]
+graph["C"] = ["A", "D"]
+graph["D"] = ["B", "C"]
+
+print("Adjacency List:")
+for vertex in graph:
+    print(vertex, ":", graph[vertex])
